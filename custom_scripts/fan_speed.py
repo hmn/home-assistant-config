d429c5ed8624b2b54318fa5194cb1b5f67092fe3
Henrik Nicolaisen <henrik@nicolaisen.co>
2019-09-29 00:32:39 +0200

    test

--- custom_scripts/fan_speed.py
+++ custom_scripts/fan_speed.py
@@ -7,7 +7,7 @@ import sys
 IPADDR = '10.254.2.248'
 PORTNUM = 4000
 # enter the data content of the UDP packet as hex
-PACKETDATA = bytes.fromhex('6d6f62696c6504030d0a')
+PACKETDATA = bytes.fromhex('6d6f62696c6504010d0a')
 # initialize a socket, think of it as a cable
 # SOCK_DGRAM specifies that this is UDP
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
