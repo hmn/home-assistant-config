84c41c439ec36bdf6bb2e67cf55434c5515f3b51
Henrik Nicolaisen <henrik@nicolaisen.co>
2021-12-13 11:45:59 +0100

    added fan status and on/off commands

--- custom_scripts/fan_control.py
+++ custom_scripts/fan_control.py
@@ -3,11 +3,50 @@ import string
 import sys
 import argparse
 
+def send_command(value):
+    """Send command to fan controller."""
+    PACKETCOMMAND = bytes.fromhex(value)
+
+    # enter the data content of the UDP packet as hex
+    #PACKETDATA = bytes.fromhex('6d6f62696c6504010d0a')
+    PACKETDATA = PACKETPREFIX + PACKETCOMMAND + PACKETPOSTFIX
+    #print(PACKETDATA)
+
+    try:
+        # initialize a socket, think of it as a cable
+        # SOCK_DGRAM specifies that this is UDP
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+        s.settimeout(10)
+
+        server_address = (IPADDR, PORTNUM)
+        #print(server_address)
+
+        # Send data
+        sent = s.sendto(PACKETDATA, server_address)
+
+        # Receive response
+        # TODO: add timeout
+        #print('waiting to receive')
+        data, server = s.recvfrom(4096)
+
+        #print('received {}'.format(data.hex()))
+        #print('received {!r}'.format(data))
+    except Exception as e:
+        raise Exception(f"Error sending command to fan controller: {e}") from e
+    finally:
+        #print('closing socket')
+        s.close()
+
+    hexstring = data.hex()
+    hexlist = [''.join(x) for x in zip(*[iter(hexstring)]*2)]
+    return hexlist
+
+
 parser = argparse.ArgumentParser(description='Siku RV fan controller.')
 parser.add_argument('--ip', help='fan ip addreee', required=True)
 parser.add_argument('--port', help='fan UDP port (default: 4000)', default=4000)
 parser.add_argument('--command', help='command to run',
-                    choices=['status', 'power', 'speed', 'direction', 'sleep', 'party'],
+                    choices=['status', 'power', 'speed', 'direction', 'sleep', 'party', 'on', 'off'],
                     required=True)
 parser.add_argument('--speed', help='speed to use',
                     choices=['01', '02', '03'],
@@ -36,46 +75,38 @@ directions = {
 
 # handle commands
 if args.command == 'status':
-    value = '01'
+    cmd = '01'
 elif args.command == 'power':
-    value = '03'
+    cmd = '03'
+elif args.command == 'on':
+    cmd = '03'
+    status_cmd = '01'
+    status_expect = '00'
+elif args.command == 'off':
+    cmd = '03'
+    status_cmd = '01'
+    status_expect = '01'
 elif args.command == 'speed':
-    value = '04' + args.speed
+    cmd = '04' + args.speed
 elif args.command == 'direction':
-    value = '06' + directions[args.direction]
+    cmd = '06' + directions[args.direction]
 elif args.command == 'sleep':
-    value = '0901'
+    cmd = '0901'
 elif args.command == 'party':
-    value = '0902'
-
-PACKETCOMMAND = bytes.fromhex(value)
-
-# enter the data content of the UDP packet as hex
-#PACKETDATA = bytes.fromhex('6d6f62696c6504010d0a')
-PACKETDATA = PACKETPREFIX + PACKETCOMMAND + PACKETPOSTFIX
-#print(PACKETDATA)
-
-# initialize a socket, think of it as a cable
-# SOCK_DGRAM specifies that this is UDP
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-s.settimeout(10)
-
-server_address = (IPADDR, PORTNUM)
-#print(server_address)
+    cmd = '0902'
+else:
+    print('invalid command')
+    exit(1)
 
 try:
-    # Send data
-    sent = s.sendto(PACKETDATA, server_address)
+    if status_cmd:
+        hexlist = send_command(status_cmd)
+        if hexlist[7] == status_expect:
+            # fan already on/off no need to run command
+            print(hexlist[7])
+            exit(0)
 
-    # Receive response
-    # TODO: add timeout
-    #print('waiting to receive')
-    data, server = s.recvfrom(4096)
-    #print('received {}'.format(data.hex()))
-    #print('received {!r}'.format(data))
-
-    hexstring = data.hex()
-    hexlist = [''.join(x) for x in zip(*[iter(hexstring)]*2)]
+    hexlist = send_command(cmd)
 
     power_value = hexlist[7]
     mode_value = hexlist[9]
@@ -99,6 +130,3 @@ try:
 except Exception as e:
     print("Error : {}".format(e))
     exit(1)
-finally:
-    #print('closing socket')
-    s.close()
