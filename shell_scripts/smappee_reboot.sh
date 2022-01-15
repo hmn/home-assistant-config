2a655b42941d603df9e464c0271c78e3eaa622c9
Henrik Nicolaisen <henrik@nicolaisen.co>
2022-01-15 23:42:46 +0100

    update curl output

--- shell_scripts/smappee_reboot.sh
+++ shell_scripts/smappee_reboot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo 'Login to smappee'
-curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" --data 'admin' http://192.168.8.113/gateway/apipublic/logon
+curl -s -X POST -H "Accept: application/json" -H "Content-Type: application/json" --data 'admin' http://192.168.8.113/gateway/apipublic/logon
+echo
 echo 'Reboot energy monitor'
-curl -X GET -H "Accept: application/json" -H "Content-Type: application/json" http://192.168.8.113/gateway/apipublic/restartSmappee?action=2
+curl -s -X GET -H "Accept: application/json" -H "Content-Type: application/json" http://192.168.8.113/gateway/apipublic/restartSmappee?action=2
+echo
 echo 'DONE'
 exit 0
