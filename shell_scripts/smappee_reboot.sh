1a87123e0241ede39b7e24ef39c621ece729c5b1
Henrik Nicolaisen <henrik@nicolaisen.co>
2022-01-15 23:36:10 +0100

    return correct exit code

--- shell_scripts/smappee_reboot.sh
+++ shell_scripts/smappee_reboot.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo 'Login to smappee'
 curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" --data 'admin' http://192.168.8.113/gateway/apipublic/logon
+echo 'Reboot energy monitor'
 curl -X GET -H "Accept: application/json" -H "Content-Type: application/json" http://192.168.8.113/gateway/apipublic/restartSmappee?action=2
+echo 'DONE'
+exit 0
